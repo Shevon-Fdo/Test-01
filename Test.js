03fc603b982fe664f8862031792cd576825b2802
Saveen Maduranga <105966451+saveen-maduranga@users.noreply.github.com>
2024-06-17 09:44:15 +0530

    Create Test.js
    Assuming there's a button <button id="myButton">Click me</button> in your HTML

---
+++ Test.js
@@ -0,0 +1,5 @@
+// Assuming there's a button <button id="myButton">Click me</button> in your HTML
+const myButton = document.getElementById("myButton");
+myButton.addEventListener("click", () => {
+    alert("Button was clicked!");
+});
